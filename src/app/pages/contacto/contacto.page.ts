1c40ffc7c3e717696db96adae6005db858c9f1f3
javivi71 <jembfh@gmail.com>
2026-07-01 14:00:02 +0200

    Persistencia (incompleto)

--- src/app/pages/contacto/contacto.page.ts
+++ src/app/pages/contacto/contacto.page.ts
@@ -1,17 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonButton, IonContent, IonHeader, IonInput, IonItem, IonTextarea, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { Preferences } from '@capacitor/preferences';
 
 @Component({
   selector: 'app-contacto',
   templateUrl: './contacto.page.html',
   styleUrls: ['./contacto.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule,IonItem,IonButton,IonTextarea,IonInput]
 })
 export class ContactoPage implements OnInit {
+correo:string = '';
+mensaje:string = '';
+enviado:boolean = false;
 
+
+  async enviar() {
+     await Preferences.set({
+      key: 'ultimoMensaje',
+      value: JSON.stringify({
+          correo:this.correo,
+          mensaje:this.mensaje
+          })
+   });
+  }
+
+  async obtenerPreferencia(key:string){
+    const result = await Preferences.get({key});
+    return result.value;
+  }
   constructor() { }
 
   ngOnInit() {
